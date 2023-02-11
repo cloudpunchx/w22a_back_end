f96c08ee094c5bb51d32715e225c909b4e20e4a9
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-11 16:37:58 -0700

    Created Get Candy function and Patch Candy function, tested and working as expected. Come back to edit response later.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 from dbhelpers import run_statement
 from dbcreds import production_mode
+from helpers import check_data
 
 app = Flask(__name__)
 
+# Clients can GET/read all candy posts
 @app.get('/api/candy')
 def get_candy():
     result = run_statement("CALL get_all_candy()")
@@ -15,6 +17,27 @@ def get_candy():
         return make_response(jsonify(response), 200)
     else:
         return make_response(jsonify(response), 500)
+
+# Clients can PATCH/edit candy name for existing candy
+# WORKING BUT NEED TO COME BACK AND EDIT RESPONSES
+@app.patch('/api/candy')
+def patch_candy_name():
+    """
+    Expects 2 Arguments:
+    candyId, candyName
+    """
+    required_data = ['candyId', 'candyName']
+    check_result = check_data(request.json, required_data)
+    if check_result != None:
+        return check_result
+    candy_id = request.json.get('candyId')
+    candy_name = request.json.get('candyName')
+    result = run_statement("CALL patch_candy(?,?)", [candy_id, candy_name])
+    if (type(result) == list):
+        return make_response(jsonify(result), 200)
+    else:
+        return make_response(jsonify(result), 500)
+
 
 if (production_mode == True):
     print("Running server in Production Mode")
