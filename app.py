16461a35705bb648a90edaf6b2238409eaca2391
cloudpunchx <siobhanbonardi@hotmail.com>
2023-02-13 19:40:22 -0700

    Post function returns correct values now, tested error handling working ok. Delete function return values tested and working as well.

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def get_candy():
         return make_response(jsonify(response), 500)
 
 # Clients can PATCH/edit candy name for existing candy
-# WORKING BUT NEED TO COME BACK AND EDIT RESPONSES
 @app.patch('/api/candy')
 def patch_candy_name():
     """
@@ -33,13 +32,12 @@ def patch_candy_name():
     candy_id = request.json.get('candyId')
     candy_name = request.json.get('candyName')
     result = run_statement("CALL patch_candy(?,?)", [candy_id, candy_name])
-    if (type(result) == list):
-        return make_response(jsonify(result), 200)
+    if result == None:
+        return make_response(jsonify(f"Successfully edited Candy: {candy_name}"), 200)
     else:
-        return make_response(jsonify(result), 500)
+        return make_response(jsonify(f"Edit unsuccessful for Candy, please verify candy ID and name."), 500)
 
 # Clients can DELETE candy by giving an ID and Name (I wanted them to use both to confirm delete)
-# circle back to fix, it completes delete and sends back the ELSE with 500 error, same with PATCH
 @app.delete('/api/candy')
 def delete_candy():
     """
@@ -53,10 +51,10 @@ def delete_candy():
     candy_id = request.json.get('candyId')
     candy_name = request.json.get('candyName')
     result = run_statement("CALL delete_candy(?,?)", [candy_id, candy_name])
-    if (type(result) == list):
-        return make_response(jsonify(result), 200)
-    else:
-        return make_response(jsonify(result), 500)
+    if result[0][0] == 1:
+        return make_response(jsonify(f"Successfully deleted candy {candy_name}."), 200)
+    elif result[0][0] == 0:
+        return make_response(jsonify(f"Delete unsuccessful for candy {candy_name}, check candy ID and name."), 500)
 
 # Clients can POST/create candy by giving a name and category
 @app.post('/api/candy')
@@ -73,9 +71,14 @@ def post_candy():
     category = request.json.get('category')
     result = run_statement("CALL post_candy(?,?)", [candy_name, category])
     if (type(result) == list):
-        return make_response(jsonify(result), 200)
+        if result[0][0] == 1:
+            return make_response(jsonify(f"Successfully posted Candy: {candy_name}."), 200)
+        elif result[0][0] == 0:
+            return make_response(jsonify(f"Post unsuccessful for candy {candy_name}, check candy ID and name."), 500)
+    elif "candy_UN_name" in result:
+        return make_response(jsonify(f"{candy_name} is a duplicate entry, please enter a unique candy name."), 400)
     else:
-        return make_response(jsonify(result), 500)
+        return "There has been an unexpected error."
 
 if (production_mode == True):
     print("Running server in Production Mode")
